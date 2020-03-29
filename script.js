9b4dffb48f5c9fd10ff4def6e46205b15b0472a5
dfel08 <dfellows68@gmail.com>
2020-03-29 08:57:11 -0700

    Link UV api

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello")
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://weatheronline2-uv-index-v1.p.rapidapi.com/api/countrycitylist");
+
+req.headers({
+	"x-rapidapi-host": "weatheronline2-uv-index-v1.p.rapidapi.com",
+	"x-rapidapi-key": "e00431f19fmshdb1dcf01d2cc529p1acb6djsnb2fd2c425430"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
