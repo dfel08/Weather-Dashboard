9e7090222c048c72cfea79f6cd0fe2535abbc0f2
dfel08 <dfellows68@gmail.com>
2020-03-29 17:38:02 -0700

    stuck on cityName variable failing to take in value

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-console.log("Hello")
-//UV API Call
-var unirest = require("unirest");
+var cityName = $("#search").attr("value");
+$(document).submit(function () {
+    event.preventDefault();
+    //Open Weather API Call
+    var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=2f0ba5199de08099b1f937f7db0ea1d8";
+    
+    
+    
+    //var UVAPI = "https://api.openweathermap.org/data/2.5/uvi?appid=2f0ba5199de08099b1f937f7db0ea1d8&lat=" + lat + "&lon=" + lon
+    //var lat = latitude
+    //var lon = longitude
 
-var req = unirest("GET", "https://weatheronline2-uv-index-v1.p.rapidapi.com/api/countrycitylist");
+    $.ajax({
+        url: openWeatherAPI,
+        method: "GET"
+    }).then(function (response) {
+        // console.log the response 
+        console.log(response)
+    });
 
-req.headers({
-	"x-rapidapi-host": "weatheronline2-uv-index-v1.p.rapidapi.com",
-	"x-rapidapi-key": "e00431f19fmshdb1dcf01d2cc529p1acb6djsnb2fd2c425430"
 });
 
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
-//Open Weather API Call
-var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=2f0ba5199de08099b1f937f7db0ea1d8";
-var cityName = $("#search").val();
 
